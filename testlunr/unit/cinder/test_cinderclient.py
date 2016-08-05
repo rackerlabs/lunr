9146b98c551cb7acf8fa3e87d249674386048f82
Aditya Purandare <aditya.p1993@hotmail.com>
2016-08-05 16:09:46 -0500

    Add unit tests for cinderclient

--- testlunr/unit/cinder/test_cinderclient.py
+++ testlunr/unit/cinder/test_cinderclient.py
@@ -78,13 +78,20 @@ class TestCinderClient(unittest.TestCase):
         self.urllib2 = MockUrllib()
         cinderclient.urllib2 = self.urllib2
         _stub_auth_response.called = False
+        self.client = cinderclient.CinderClient(
+            username='fake',
+            password='fake',
+            auth_url='http://auth:5000',
+            cinder_url='http://cinder:8776',
+            tenant_id='fake',
+            admin_tenant_id='fake-admin',
+            rax_auth=False
+        )
 
     def tearDown(self):
         cinderclient.urllib2 = self._orig_urllib2
 
     def test_rax_auth(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776', rax_auth=True)
 
         def auth_request(req):
             expected_url = 'http://auth:5000/v2.0/tokens'
@@ -95,7 +102,8 @@ class TestCinderClient(unittest.TestCase):
                     'passwordCredentials': {
                         'username': 'fake',
                         'password': 'fake',
-                    }
+                    },
+                    'tenantId': 'fake'
                 }
             })
             self.assertEquals(req.data, expected_data)
@@ -134,15 +142,12 @@ class TestCinderClient(unittest.TestCase):
 
         self.urllib2.responses = iter([auth_request, update_request])
 
-        client.update('volumes', 'fake', 'available')
+        self.client.update('volumes', 'fake', 'available')
 
         self.assert_(auth_request.called)
         self.assert_(update_request.called)
 
     def test_keystone_auth(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def auth_request(req):
             expected_url = 'http://auth:5000/v2.0/tokens'
@@ -193,7 +198,7 @@ class TestCinderClient(unittest.TestCase):
 
         self.urllib2.responses = iter([auth_request, update_request])
 
-        client.update('volumes', 'fake', 'available')
+        self.client.update('volumes', 'fake', 'available')
 
         self.assert_(auth_request.called)
         self.assert_(update_request.called)
@@ -213,9 +218,6 @@ class TestCinderClient(unittest.TestCase):
         self.assertEquals(client.password, 'admin')
 
     def test_update_volume(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def update_request(req):
             # should be volumes update
@@ -237,14 +239,11 @@ class TestCinderClient(unittest.TestCase):
 
         self.urllib2.responses = iter([_stub_auth_response, update_request])
 
-        client.update('volumes', 'vol1', 'available')
+        self.client.update('volumes', 'vol1', 'available')
 
         self.assert_(update_request.called)
 
     def test_update_snapshot(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def update_request(req):
             expected_url = 'http://cinder:8776/v1/fake/snapshots/snap1/action'
@@ -265,14 +264,11 @@ class TestCinderClient(unittest.TestCase):
 
         self.urllib2.responses = iter([_stub_auth_response, update_request])
 
-        client.update('snapshots', 'snap1', 'available')
+        self.client.update('snapshots', 'snap1', 'available')
 
         self.assert_(update_request.called)
 
     def test_force_delete_volume(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def force_delete_request(req):
             expected_url = 'http://cinder:8776/v1/fake/volumes/vol2/action'
@@ -290,14 +286,11 @@ class TestCinderClient(unittest.TestCase):
         self.urllib2.responses = iter([_stub_auth_response,
                                        force_delete_request])
 
-        client.force_delete('volumes', 'vol2')
+        self.client.force_delete('volumes', 'vol2')
 
         self.assert_(force_delete_request.called)
 
     def test_force_delete_snapshot(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def force_delete_request(req):
             expected_url = 'http://cinder:8776/v1/fake/snapshots/snap2/action'
@@ -315,14 +308,11 @@ class TestCinderClient(unittest.TestCase):
         self.urllib2.responses = iter([_stub_auth_response,
                                        force_delete_request])
 
-        client.force_delete('snapshots', 'snap2')
+        self.client.force_delete('snapshots', 'snap2')
 
         self.assert_(force_delete_request.called)
 
     def test_auth_retry(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def unauthorized_request(req):
             raise urllib2.HTTPError(req.get_full_url(), 401, 'Unauthorized',
@@ -335,17 +325,14 @@ class TestCinderClient(unittest.TestCase):
                                        _stub_auth_response, update_request])
 
         # set invalid token
-        client.token = 'invalid'
-        client.update('volumes', 'fake', 'available')
+        self.client.token = 'invalid'
+        self.client.update('volumes', 'fake', 'available')
         self.assert_(unauthorized_request.called)
         self.assert_(update_request.called)
         # new token should be validated
-        self.assertEquals(client.token, 'fake')
+        self.assertEquals(self.client.token, 'fake')
 
     def test_auth_retry_fails_after_five_attempts(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def unauthorized_request(req):
             raise urllib2.HTTPError(req.get_full_url(), 401, 'Unauthorized',
@@ -358,14 +345,11 @@ class TestCinderClient(unittest.TestCase):
             _stub_auth_response, unauthorized_request,
             _stub_auth_response, unauthorized_request,
         ])
-        self.assertRaises(cinderclient.CinderError, client.update,
+        self.assertRaises(cinderclient.CinderError, self.client.update,
                           'volumes', 'fake', 'available')
         self.assertEquals(unauthorized_request.count, 5)
 
     def test_auth_retry_unable_to_auth(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def unauthorized_request(req):
             raise urllib2.HTTPError(req.get_full_url(), 401, 'Unauthorized',
@@ -376,14 +360,11 @@ class TestCinderClient(unittest.TestCase):
             unauthorized_request, unauthorized_request,
             unauthorized_request, unauthorized_request,
         ])
-        self.assertRaises(cinderclient.CinderError, client.update,
+        self.assertRaises(cinderclient.CinderError, self.client.update,
                           'volumes', 'fake', 'available')
         self.assertEquals(unauthorized_request.count, 5)
 
     def test_snapshot_progress(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
         new_progress = "something%"
 
         def snapshot_progress_request(req):
@@ -402,14 +383,11 @@ class TestCinderClient(unittest.TestCase):
         self.urllib2.responses = iter([_stub_auth_response,
                                        snapshot_progress_request])
 
-        client.snapshot_progress('snap2', new_progress)
+        self.client.snapshot_progress('snap2', new_progress)
 
         self.assert_(snapshot_progress_request.called)
 
     def test_update_volume_metadata(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def metadata_request(req):
             expected_url = 'http://cinder:8776/v1/' \
@@ -431,14 +409,11 @@ class TestCinderClient(unittest.TestCase):
 
         self.urllib2.responses = iter([_stub_auth_response, metadata_request])
 
-        client.update_volume_metadata('volume_id', {'key': 'value'})
+        self.client.update_volume_metadata('volume_id', {'key': 'value'})
 
         self.assert_(metadata_request.called)
 
     def test_delete_volume_metadata(self):
-        client = cinderclient.CinderClient('fake', 'fake', 'http://auth:5000',
-                                           'http://cinder:8776',
-                                           tenant_id='fake', rax_auth=False)
 
         def metadata_request(req):
             expected_url = 'http://cinder:8776/v1/' \
@@ -454,10 +429,69 @@ class TestCinderClient(unittest.TestCase):
 
         self.urllib2.responses = iter([_stub_auth_response, metadata_request])
 
-        client.delete_volume_metadata('volume_id', 'key')
+        self.client.delete_volume_metadata('volume_id', 'key')
 
         self.assert_(metadata_request.called)
 
+    def test_get_volume(self):
+
+        def get_request(req):
+            expected_url = 'http://cinder:8776/v1/fake/volumes/vol1'
+            self.assertEquals(req.get_full_url(), expected_url)
+            self.assertEquals(req.get_method(), 'GET')
+            expected_headers = {
+                'Content-type': 'application/json',
+                'X-auth-token': 'fake',
+            }
+            self.assertEquals(req.headers, expected_headers)
+            return MockResponse()
+
+        self.urllib2.responses = iter([_stub_auth_response,
+                                       get_request])
+
+        self.client.get_volume('vol1')
+
+        self.assert_(get_request.called)
+
+    def test_delete_volume(self):
+
+        def delete_request(req):
+            expected_url = 'http://cinder:8776/v1/fake/volumes/vol1'
+            self.assertEquals(req.get_full_url(), expected_url)
+            self.assertEquals(req.get_method(), 'DELETE')
+            expected_headers = {
+                'Content-type': 'application/json',
+                'X-auth-token': 'fake',
+            }
+            self.assertEquals(req.headers, expected_headers)
+            return MockResponse()
+
+        self.urllib2.responses = iter([_stub_auth_response,
+                                       delete_request])
+
+        self.client.delete_volume('vol1')
+
+        self.assert_(delete_request.called)
+
+    def test_list_volumes(self):
+
+        def get_request(req):
+            expected_url = 'http://cinder:8776/v1/fake/volumes/detail'
+            self.assertEquals(req.get_full_url(), expected_url)
+            self.assertEquals(req.get_method(), 'GET')
+            expected_headers = {
+                'Content-type': 'application/json',
+                'X-auth-token': 'fake',
+            }
+            self.assertEquals(req.headers, expected_headers)
+            return MockResponse()
+
+        self.urllib2.responses = iter([_stub_auth_response,
+                                       get_request])
+
+        self.client.list_volumes()
+
+        self.assert_(get_request.called)
 
 if __name__ == "__main__":
     unittest.main()
